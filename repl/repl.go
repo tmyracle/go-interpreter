24dbb6fb786f160b711e01c44274b245ef3ab2bc
Tyler Myracle <tyler.myracle@gmail.com>
2024-02-04 17:06:11 -0600

    basic REPL

---
+++ repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/tmyracle/go-interpreter/lexer"
+	"github.com/tmyracle/go-interpreter/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Print(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
